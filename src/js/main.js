89e48a0c3b5ac21df0dc5e275efbb65ad728ee11
basile-kamm <another-black-kitty@proton.me>
2024-03-13 10:53:28 +0100

    Footer + code intégration

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   selects.forEach(function (select) {
     select.addEventListener("click", toggleActive);
+    select.addEventListener("mouseenter", removeActiveFromOthers);
   });
 
   document.addEventListener("click", function (event) {
@@ -14,6 +15,34 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   function toggleActive() {
+    const clickedSelect = this;
+    selects.forEach(function (select) {
+      if (select !== clickedSelect) {
+        select.classList.remove("is-active");
+      }
+    });
     this.classList.toggle("is-active");
   }
+
+  function removeActiveFromOthers() {
+    const hoveredSelect = this;
+    selects.forEach(function (select) {
+      if (select !== hoveredSelect) {
+        select.classList.remove("is-active");
+      }
+    });
+  }
 });
+
+const inputName = document.querySelector(".code-input");
+
+function hasValue() {
+  const inputValue = inputName.value;
+  if (inputValue) {
+    inputName.classList.add("has-value");
+  } else {
+    inputName.classList.remove("has-value");
+  }
+}
+
+inputName.addEventListener("blur", hasValue);
